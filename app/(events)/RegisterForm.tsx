00cc62af79b68c3b86b143bae03a069713238cf5
sammimagg <sammimagg@gmail.com>
2023-04-20 20:58:16 +0000

    Lagi lagi

--- app/(events)/RegisterForm.tsx
+++ app/(events)/RegisterForm.tsx
@@ -1,33 +1,34 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { useSession } from "next-auth/react";
 import { registerForEvent } from "../api/event";
 import { useState } from "react";
 
+export default function RegisterForm() {
+  const {data: session}= useSession();
+  const [comment, setComment] = useState<string>("");
+  const [event, setEvent] = useState<Event | null>(null);
 
-export default function RegisterForm(){
-        const [comment, setComment] = useState<string>('');
-        const [event, setEvent] = useState<Event | null>(null);
-
-        const handleSubmit = async (e: React.FormEvent) => {
-            e.preventDefault();
-            const response = await userToEvent(event, comment);
-        }
-
-    async function userToEvent(eventId: any, comment: string) {
-        const session = await getServerSession(authOptions);
-        if(session?.user){
-          const name = session.user.username;
-          registerForEvent(eventId, name, comment)
-        }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (session) {
+      const response = await userToEvent(event, comment);
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <p>Comment:</p>
-            <textarea 
-                onChange={(e) => setComment(e.target.value)}
-                value={comment}
-            />
-            <button type="submit">Register for event</button>
-        </form >
-    )
+  };
+
+  async function userToEvent(eventId: any, comment: string) {
+    if (session?.user) {
+      const name = session.user.username;
+      registerForEvent(eventId, name, comment);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Comment:</p>
+      <textarea
+        onChange={(e) => setComment(e.target.value)}
+        value={comment}
+      />
+      <button type="submit">Register for event as {session?.user.name}</button>
+    </form>
+  );
 }
