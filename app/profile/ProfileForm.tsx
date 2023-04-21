40753f43d25345c4b5ddcc797d9370384b657b64
hakoningir <hir12@hi.is>
2023-04-21 18:56:59 +0000

    sma af þvi sem eg var að gera fyrir atvikið

--- app/profile/ProfileForm.tsx
+++ app/profile/ProfileForm.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 export default function ProfileForm(): JSX.Element {
   const {data: session} = useSession();
   const [imageUrl, setImageUrl] = useState<string>('');
+  const [image, setImage] = useState<File | null>(null);
 
   const id: string = session?.user.id ?? '';
   const accessToken: string = session?.user.access_token ?? '';
@@ -26,12 +27,19 @@ export default function ProfileForm(): JSX.Element {
       console.error(error);
     }
   };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setImage(file || null);
+  };
+  
   return (
     <div>
       <h2>{session?.user.username}</h2>
-      <h2>{session?.user.name}</h2>
+      {/* <h2>{session?.user.name}</h2> */}
       <form onSubmit={handleNewProfilePicture}>
         <PhotoBox imageUrl={imageUrl} width={300} height={300} />
+        <input type="file" onChange={handleImageChange} accept="image/png, image/jpg" />        
         <button type='submit'>New profile picture</button>
       </form>
       <Link href="/">
