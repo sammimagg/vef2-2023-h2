a7d603f775806fd711896468c1c0c047e98dadbe
Hákon Ingi <hir12@hi.is>
2023-04-22 23:41:55 +0000

    navbar fyrir admin

--- app/(events)/[slug]/registeredUsers.tsx
+++ app/(events)/[slug]/registeredUsers.tsx
@@ -73,5 +73,4 @@ function  UserRegistered({ registered }: { registered: Registration }) {
       </div>
     )
   }
-
 }
--- app/nav.tsx
+++ app/nav.tsx
@@ -3,14 +3,35 @@ import LogOut from "./Logout-btn"
 import { authOptions } from "../pages/api/auth/[...nextauth]"
 import styles from "./page.module.css"
 import Link from "next/link";
+import defaultImage from'../public/images/default user.png'
+import { useSession } from "next-auth/react";
+
 export default async function NavBar() {
 
     const session = await getServerSession(authOptions)
-    if(session) {
+    console.log(session?.user.profile_picture)
+    if(session?.user.admin){
+        return (
+            <nav className={styles.userNav}>
+                <div>
+                    <img 
+                        src={session.user.profile_picture} 
+                        alt={session.user.name} 
+                        className={styles.adminPicture}
+                        />
+                    <p>{session.user.name}</p>
+                </div>
+                <Link href={"/"} className={styles.homePageLink}>Home page</Link>
+                <Link href={"/admin"} className={styles.adminPageLink}>Admin panel</Link>
+                <LogOut/>
+            </nav>
+        );
+    }
+    else if(session) {
         return (
             <nav className={styles.userNav}>
                 <p>{session.user.name}</p>
-                <Link className="profileLink" href="profile">
+                <Link className={styles.profileLink} href="profile">
                     Profile page
                 </Link>
                 <LogOut/>
