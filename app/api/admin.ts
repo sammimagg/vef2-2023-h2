33ef376a035a7340226a58fe62d8d0e5e2076955
Hákon Ingi <hir12@hi.is>
2023-04-23 00:22:30 +0000

    login takki birtist ekki þegar user er loggaður inn

--- app/api/admin.ts
+++ app/api/admin.ts
@@ -182,7 +182,8 @@ export async function createEventAPI(
     name: string,
     description: string,
     location: string,
-    url: string
+    url: string,
+    slug: string
   ) {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/admin`, {
       method: 'POST',
